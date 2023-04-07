b85fa21548c89e26e5695c87e13cd667ac6ac895
ahmet cançelik <ahmetcancelik27@hotmail.com>
2023-04-08 01:58:24 +0300

    backroads-application

---
+++ src/data.js
@@ -0,0 +1,109 @@
+import tourImg1 from './images/tour-1.jpeg'
+import tourImg2 from './images/tour-2.jpeg'
+import tourImg3 from './images/tour-3.jpeg'
+import tourImg4 from './images/tour-4.jpeg'
+import tourImg5 from './images/tour-5.jpeg'
+import tourImg6 from './images/tour-6.jpeg'
+
+export const pageLinks = [
+  { id: 1, href: '#home', text: 'home' },
+  { id: 2, href: '#about', text: 'about' },
+  { id: 3, href: '#services', text: 'services' },
+  { id: 4, href: '#tours', text: 'tours' },
+]
+
+export const socialLinks = [
+  { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
+  { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
+  { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
+]
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'saving cost',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree fa-fw',
+    title: 'endless hiking',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-socks fa-fw',
+    title: 'amazing comfort',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    img: tourImg1,
+    date: 'august 13th, 2020',
+    title: 'Tibet Adventure',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    icon: 'fas fa-map',
+    country: 'türkiye',
+    day: '27 days',
+    cost: '$2700',
+  },
+  {
+    id: 2,
+    img: tourImg2,
+    date: 'july 12th, 2020',
+    title: 'Tibet Adventure',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    icon: 'fas fa-map',
+    country: 'somalia',
+    day: '11 days',
+    cost: '$1000',
+  },
+  {
+    id: 3,
+    img: tourImg3,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    icon: 'fas fa-map',
+    country: 'egypt',
+    day: '16 days',
+    cost: '$1500',
+  },
+  {
+    id: 4,
+    img: tourImg4,
+    date: 'december 23th, 2020',
+    title: 'Tibet Adventure',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    icon: 'fas fa-map',
+    country: 'denmark',
+    day: '5 days',
+    cost: '$1900',
+  },
+  {
+    id: 5,
+    img: tourImg5,
+    date: 'september 10th, 2020',
+    title: 'Tibet Adventure',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    icon: 'fas fa-map',
+    country: 'italy',
+    day: '9 days',
+    cost: '$2000',
+  },
+  {
+    id: 6,
+    img: tourImg6,
+    date: 'august 20th, 2020',
+    title: 'Tibet Adventure',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    icon: 'fas fa-map',
+    country: 'germany',
+    day: '8 days',
+    cost: '$1750',
+  },
+]
